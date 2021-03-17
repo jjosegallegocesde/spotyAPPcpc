f940c3847082686888e317ec97d796eae9fe4d12
Juan <juan.gallegomesa@gmail.com>
2021-03-17 16:50:08 -0500

    cambios de footer

---
+++ cambioMenu.js
@@ -0,0 +1,24 @@
+/*let titulo=document.getElementById("titulo");
+titulo.textContent="BUENAS TARDES";
+
+let imagen=document.getElementById("foto1");
+imagen.src="img/fsoc.jpg";*/
+
+window.addEventListener("scroll",cambiarColor);
+
+function cambiarColor(){
+
+    let menu=document.getElementById("menu");
+
+    if(document.body.scrollTop||document.documentElement.scrollTop==0){
+
+        menu.classList.add("bg-dark");
+        menu.classList.remove("fondo2");
+
+    }else{
+        menu.classList.add("fondo2");
+        menu.classList.remove("bg-dark");
+    }
+
+}
+
