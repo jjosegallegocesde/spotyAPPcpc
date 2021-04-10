1a81e7c9cf756555e8de1ac11f94cf48a8470015
Juan <juan.gallegomesa@gmail.com>
2021-04-10 09:39:15 -0500

    se consume API

---
+++ conexion.js
@@ -0,0 +1,26 @@
+//Pasos para conectarme al servidor de spotify
+
+//1. Identifico la URL DEL SERVIDOR
+let url="https://api.spotify.com/v1/artists/3YcBF2ttyueytpXtEzn1Za/top-tracks?market=us";
+
+//2. Si es necesario identifico un TOKEN de autorización
+let token="Bearer BQCBAl7tQ2MbFCxLMvWcdBYkCOsKgjHj_yy9KjDEcuaeHmZAwYzbhfe2YsqATRkS6znTzYCKV64Yppj6Cg1hXwu5LojV_2AnpS_ZoFUdvwyYp8xEqkxNSZHDYLw3Lw_IJeUk9Qg48ctXI22w9KY";
+
+//3. Configuro el metodo (GET/POST/PUT/DELETE)
+// las cabeceras del envio de datos
+let parametros={
+    method:"GET",
+    headers:{"Authorization":token}
+}
+
+//4. Establecer la conexión al servidor de spotify
+fetch(url,parametros)
+.then(respuesta=>respuesta.json())
+.then(datos=>seleccionarInformacion(datos))
+
+//5. Crear una funcion para seleccionar información
+function seleccionarInformacion(datos){
+
+    console.log(datos);
+
+}
