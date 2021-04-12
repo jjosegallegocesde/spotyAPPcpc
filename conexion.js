d6d75017331263e6fa08db180d40a00e1b80e0f1
Juan <juan.gallegomesa@gmail.com>
2021-04-12 17:56:03 -0500

    se obtienen datos de spotify

--- conexion.js
+++ conexion.js
@@ -4,7 +4,7 @@
 let url="https://api.spotify.com/v1/artists/3YcBF2ttyueytpXtEzn1Za/top-tracks?market=us";
 
 //2. Si es necesario identifico un TOKEN de autorización
-let token="Bearer BQCBAl7tQ2MbFCxLMvWcdBYkCOsKgjHj_yy9KjDEcuaeHmZAwYzbhfe2YsqATRkS6znTzYCKV64Yppj6Cg1hXwu5LojV_2AnpS_ZoFUdvwyYp8xEqkxNSZHDYLw3Lw_IJeUk9Qg48ctXI22w9KY";
+let token="Bearer BQBk2g_mIeoWEDXHhm9SGjIPO_FmWBXuF_bgdhuZFQw9JPnywmZZay3DcxB53L_vjd856cBZDrJJA8fuYDUJzF4ZzErhNUYJvcBb8ZkiKV8A1Pt1j0E4VpOAV3Q44sYz7Zh0sxUedWej2nwKWu0";
 
 //3. Configuro el metodo (GET/POST/PUT/DELETE)
 // las cabeceras del envio de datos
@@ -21,6 +21,14 @@ fetch(url,parametros)
 //5. Crear una funcion para seleccionar información
 function seleccionarInformacion(datos){
 
-    console.log(datos);
+    
+    console.log(datos.tracks[0].album.images[0].url);//Imagen del album
+    console.log(datos.tracks[0].preview_url); //MP3 cancion
+    console.log(datos.tracks[0].name); //nombre cancion
+
+
+    let cancion=document.getElementById("cancion1");
+    cancion.src=datos.tracks[0].preview_url;
+    
 
 }
---
+++ prueba.js
@@ -0,0 +1,9 @@
+let bolsa={
+
+    proteinas:"carne",
+    vegetales:["cebolla","tomate","lechuga"],
+    complementos:["salsa de tomate","queso","mostaza"],
+    carbohidrato:"pan"
+
+}
+console.log(bolsa.vegetales);
